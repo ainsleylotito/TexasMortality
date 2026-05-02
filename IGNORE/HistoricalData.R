c79954c5da4b0740ab570cf4292e45868bcd2eff
Ainsley Lotito <ainsleymlotito@gmail.com>
2026-05-02 15:21:57 -0600

    historical data update.

--- IGNORE/HistoricalData.R
+++ IGNORE/HistoricalData.R
@@ -26,7 +26,8 @@ pre_18 <- pre_18 %>%
          month = yearmonth(month_year),
          Deaths = na_if(as.character(Deaths), "Suppressed"),
          Deaths = as.numeric(Deaths),) %>% 
-  select(-Notes,-Population,-"Crude.Rate", -Month, -"Month.Code") 
+  select(-Notes,-Population,-"Crude.Rate", -Month, -"Month.Code")  
+head(pre_18)
 
 #we need to wait to make it a time series object until we've merged the data from before!! 
 f_post_18 <- read.csv("./CleanData/monthly_f.csv") 
@@ -36,8 +37,7 @@ f_post_18 <- f_post_18 %>%
          month = yearmonth(month_year)) %>% 
   select(-sex,-time)
 head(f_post_18)  
-f_post_18 %>% 
-  slice(15:20) 
+tail(f_post_18)
 
 
 range(pre_18$month_year)
@@ -68,15 +68,14 @@ has_gaps(f_ts)
 # show where gaps occur
 scan_gaps(f_ts) 
  
-#I rememer now! we subtracted the deaths from men to get the female total.
-#that's why it's different. 
+
 
 f_arima <- fit_arima(f_ts) 
 
 report(f_arima)
 
 
-fc_f <- counterfactual_forecast(f_ts, "2021-06-01", 40)
+fc_f <- counterfactual_forecast(f_ts, "2021-06-01", 43)
 
 plot_counterfactual(
   fc_f,
@@ -89,4 +88,28 @@ plot_counterfactual(
 
 pre_period %>% 
   summarize(mean= mean(Deaths, na.rm=TRUE),
-            sd= sd(Deaths, na.rm= TRUE))
+            sd= sd(Deaths, na.rm= TRUE)) 
+
+
+#now let's try customizing the graph:  
+
+autoplot(fc_f, f_ts, level = 95) +
+  geom_vline(
+    xintercept = make_yearmonth(2021, 6),
+    linetype = "dashed",
+    color = "grey40"
+  ) + 
+  scale_x_yearmonth(date_breaks = "6 months", date_labels = "%b %Y") + 
+  labs( y = "Monthly Female Assault Deaths", x = "Month" ) +
+ coord_cartesian(xlim = c(make_yearmonth(1999, 1), make_yearmonth(2024, 12)))+
+  theme_minimal() + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  annotate("text",
+           x = make_yearmonth(2021, 6),
+           y = 12,
+           label = "Bill 8 Ruling (June 2021)",
+           vjust = -1,
+           angle = 90,
+           size = 3) 
+
+
